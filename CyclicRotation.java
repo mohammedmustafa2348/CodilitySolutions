ca9286519d575b9b93ea2d43d685c67be12381e1
Mohammed Mustafa <mohd.mustafa488@gmail.com>
2019-07-12 14:54:28 +0200

    Create CyclicRotation.java

---
+++ CyclicRotation.java
@@ -0,0 +1,31 @@
+public class CyclicRotation {
+
+    public static void main(String[] args) {
+        Scanner inScanner = new Scanner(System.in);
+        int K = inScanner.nextInt();
+        int N = inScanner.nextInt();
+        int []A = new int[N];
+        for(int i=0; i < N; i++) {
+            A[i] = inScanner.nextInt();
+        }
+        for (int i :solution(A, K)){
+            System.out.print(i + " ");
+        }
+        inScanner.close();
+    }
+
+    
+    public static int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        if(A.length > 0) {
+            for(int i= 0; i < K; i++){
+                int temp = A[A.length-1];
+                for(int j=A.length-2; j>=0; j--){
+                    A[j+1] = A[j];
+                }
+                A[0]=temp;
+            }
+        }
+        return A;
+    }
+}
